5660c383420c2938ba9b1d8dfb8659ca48f3dc02
xukun <xukun@lilith.sh>
2019-10-08 14:41:39 +0800

    v0.1

---
+++ Assets/PoolManager/Example/CubePooledObject.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Framework
+{
+    public class CubePooledObject : GenericMonoPooledObject
+    {
+        public float m_LifeTime = 5;
+
+        private float m_StartTime;
+
+        void Awake()
+        {
+            m_StartTime = Time.time;
+        }
+
+        void Update()
+        {
+            if(Time.time - m_StartTime > m_LifeTime)
+            {
+                ReturnToPool();
+            }
+        }
+
+        public override void OnGet()
+        {
+            base.OnGet();
+
+            transform.localScale = new Vector3(Random.Range(0.5f, 3), Random.Range(0.5f, 3), Random.Range(0.5f, 3));
+            transform.localRotation = Random.rotation;
+            transform.localPosition = Random.insideUnitSphere * 5;
+
+            m_StartTime = Time.time;
+        }
+    }
+}
---
+++ Assets/PoolManager/Example/DebugPool.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Framework;
+
+public class DebugPool : MonoBehaviour
+{
+    public CubePooledObject Prefab;
+
+    private CubePool m_Pool;
+
+    public float SpawnDelay = 3;
+    private float m_TimeSinceLastSpawn;
+
+    void Start()
+    {
+        m_Pool = gameObject.GetComponent<CubePool>();
+        if (m_Pool == null)
+        {
+            m_Pool = gameObject.AddComponent<CubePool>();
+            m_Pool.PrefabAsset = Prefab;
+        }
+
+        Spawn();
+    }
+
+    void Update()
+    {
+        //m_TimeSinceLastSpawn += Time.deltaTime;
+        //if (m_TimeSinceLastSpawn > SpawnDelay)
+        //{
+        //    Spawn();
+
+        //    m_TimeSinceLastSpawn = 0;
+        //}
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        CubePooledObject obj = (CubePooledObject)m_Pool.GetObject();
+        if(obj != null)
+            obj.transform.parent = transform;
+
+        CubePooledObject o = (CubePooledObject)m_Pool.GetObject(Vector3.zero);
+    }
+}
