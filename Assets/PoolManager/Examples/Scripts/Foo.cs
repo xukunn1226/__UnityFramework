346b144636537cc01ec993ef319f2be7917b81bc
xukun <xukun@lilith.sh>
2019-10-10 20:07:01 +0800

    Foo

---
+++ Assets/PoolManager/Examples/Scripts/Foo.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Framework;
+
+public class Foo : IPooledObject
+{
+    static private ObjectPool<Foo> m_Pool;
+
+    public void OnInit()
+    {
+        Debug.Log("Foo::OnInit");
+    }
+
+    public void OnGet()
+    {
+        Debug.Log("Foo::OnGet");
+    }
+
+    public void OnRelease()
+    {
+        Debug.Log("Foo::OnRelease");
+    }
+
+    public void ReturnToPool()
+    {
+        Debug.Log("Foo::ReturnPool");
+        //throw new System.NotImplementedException();
+    }
+
+    public IPool Pool { get; set; }
+
+    public static Foo Get()
+    {
+        if(m_Pool == null)
+        {
+            m_Pool = new ObjectPool<Foo>(1);
+        }
+
+        Foo f = (Foo)m_Pool.Get();
+        return f;
+    }
+
+    public static void Release(Foo f)
+    {
+        if (m_Pool == null)
+            return;
+
+        m_Pool.Return(f);
+    }
+}
