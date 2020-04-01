d0ccf4ed878821c93e7b743040ca18778d486b88
xukun <xukunn1226@gmail.com>
2020-04-01 23:30:24 +0800

    22

--- Assets/Scripts/PoolManager/Runtime/ObjectPool.cs
+++ Assets/Scripts/PoolManager/Runtime/ObjectPool.cs
@@ -10,13 +10,13 @@ namespace Cache
     /// <typeparam name="T"></typeparam>
     public sealed class ObjectPool<T> : IPool where T : IPooledObject, new()
     {
-        private Stack<T>            m_Stack;
+        private Stack<T>            m_DeactiveObjects;
 
         public int                  countAll        { get; private set; }
 
         public int                  countActive     { get { return countAll - countInactive; } }
 
-        public int                  countInactive   { get { return m_Stack.Count; } }
+        public int                  countInactive   { get { return m_DeactiveObjects.Count; } }
         
         public ObjectPool(int initSize = 0)
         {
@@ -29,24 +29,24 @@ namespace Cache
 
         private void InitPool(int initSize)
         {
-            if (m_Stack == null)
+            if (m_DeactiveObjects == null)
             {
-                m_Stack = new Stack<T>(initSize);
+                m_DeactiveObjects = new Stack<T>(initSize);
             }
 
-            for (int i = 0; i < initSize; ++i)
-            {
-                T element = new T();
-                element.OnInit();
-                m_Stack.Push(element);
-                ++countAll;
-            }
+            //for (int i = 0; i < initSize; ++i)
+            //{
+            //    T element = new T();
+            //    element.OnInit();
+            //    m_Stack.Push(element);
+            //    ++countAll;
+            //}
         }
 
         public IPooledObject Get()
         {
             T element;
-            if (m_Stack.Count == 0)
+            if (m_DeactiveObjects.Count == 0)
             {
                 element = new T();
                 element.OnInit();
@@ -54,7 +54,7 @@ namespace Cache
             }
             else
             {
-                element = m_Stack.Pop();
+                element = m_DeactiveObjects.Pop();
             }
             element.OnGet();
 
@@ -66,18 +66,18 @@ namespace Cache
             if (element == null)
                 return;
 
-            m_Stack.Push((T)element);
+            m_DeactiveObjects.Push((T)element);
             element.OnRelease();
         }
 
         public void Clear()
         {
-            m_Stack.Clear();
+            m_DeactiveObjects.Clear();
         }
 
         public void Trim()
         {
-            m_Stack.TrimExcess();
+            m_DeactiveObjects.TrimExcess();
         }
     }
 }
