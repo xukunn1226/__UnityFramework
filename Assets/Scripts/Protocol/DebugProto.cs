178b00be85d41ab21b0fecb43a29285319010332
xukun <xukunn1226@gmail.com>
2020-04-14 13:53:20 +0800

    1

---
+++ Assets/Scripts/Protocol/DebugProto.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NetProtocol;
+using Google.Protobuf;
+
+public class DebugProto : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StoreRequest toServer = new StoreRequest();
+        toServer.Name = "ABC";
+        toServer.Num = 3;
+        toServer.Result = 1;
+        toServer.MyList.Add("x");
+        toServer.MyList.Add("j");
+        byte[] data = toServer.ToByteArray();
+
+        StoreRequest toClient = new StoreRequest();
+        toClient = (StoreRequest)StoreRequest.Descriptor.Parser.ParseFrom(data);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
