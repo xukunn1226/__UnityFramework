513e6995cb1225bd8d536805fadfe864afdef909
xukun <xukunn1226@gmail.com>
2020-04-16 19:03:54 +0800

    33

--- Assets/GamePlay/Runtime/ResourceManager.cs
+++ Assets/GamePlay/Runtime/ResourceManager.cs
@@ -2,8 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Framework.AssetManagement.Runtime;
+using System;
 
 public class ResourceManager : MonoBehaviour
 {
-    private AssetManager m_AssetManager;
+    static internal ResourceManager Instance { get; private set; }
+
+    private AssetManager    m_AssetManager;
+
+    [SerializeField]
+    private LoaderType      m_LoaderType;
+
+    [SerializeField]
+    private string          m_RootPath          = "Deployment/AssetBundles";
+
+    static private bool     k_bDynamicLoad;                                                             // true: dynamic loading AssetManager; false: static loading AssetManager
+
+    internal LoaderType     loaderType
+    {
+        get
+        {
+#if UNITY_EDITOR
+            if (Instance != null)
+            {
+                return m_LoaderType;
+            }
+            return LoaderType.FromEditor;
+#else
+            return LoaderType.FromAB;       // 移动平台强制AB加载
+#endif
+        }
+    }
+
+
+    private void Awake()
+    {
+        // 已有AssetManager，则自毁
+        if (FindObjectsOfType<ResourceManager>().Length > 1)
+        {
+            DestroyImmediate(this);
+            throw new Exception("AssetManager has already exist...");
+        }
+
+        Instance = this;
+
+        //if (!k_bDynamicLoad)
+        //    InternalInit(m_LoaderType, m_RootPath);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        if (loaderType == LoaderType.FromAB)
+        {
+            // 确保应用层持有的AssetLoader(Async)释放后再调用
+            //AssetBundleManager.Uninit();
+        }
+        Instance = null;
+    }
 }
