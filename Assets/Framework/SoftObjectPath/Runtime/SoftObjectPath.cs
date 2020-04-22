4e548eb6225606e5f6affa4339ced4fa1aaf3767
xukun <xukunn1226@gmail.com>
2020-04-23 02:03:33 +0800

    init SoftObjectPath

---
+++ Assets/Framework/SoftObjectPath/Runtime/SoftObjectPath.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework.SoftObjectPath
+{
+    public class SoftObjectPath : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
