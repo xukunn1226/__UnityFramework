e182334ee7fe80246ea3918d3ebce0b42b311e54
xukun <xukunn1226@gmail.com>
2020-04-30 14:24:37 +0800

    1

--- Assets/Framework/Core/PoolManager/Runtime/LRUPool.cs
+++ Assets/Framework/Core/PoolManager/Runtime/LRUPool.cs
@@ -2,25 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Framework.Cache
 {
-    public class LRUPool : MonoPoolBase
+    public class LRUPool : MonoBehaviour, IPool
     {
-        public Type m_Type;
+        [Range(1, 100)]
+        public int Capacity;
+
+        int IPool.countAll { get; }
+
+        int IPool.countOfUsed { get; }
+
+        int IPool.countOfUnused { get; }
+
+        public IPooledObject Get() { return null; }
+
+        public void Return(IPooledObject item) { }
+
+        public void Clear()
+        {
+        }
+
+        void IPool.Trim() { }
+
+        private void Awake()
+        {
+            transform.position = Vector3.zero;
+            transform.rotation = Quaternion.identity;
+            transform.localScale = Vector3.one;
+        }
+
+        private void Start()
+        {
+            // 
+            if (PoolManager.Instance.gameObject != transform.root.gameObject)
+            {
+                transform.parent = PoolManager.Instance.transform;
+            }
+        }
         
-        public override int countAll { get; }
-
-        public override int countOfUsed { get; }
-
-        public override int countOfUnused { get; }
-
-        public override IPooledObject Get() { return null; }
-
-        public override void Return(IPooledObject item) { }
-
-        public override void Clear() { }
-
-        public override void Trim() { }
+        private void OnDestroy()
+        {
+            //if (!manualUnregisterPool)
+            //    PoolManager.RemoveMonoPool(this);
+        }
     }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(LRUPool))]
+    public class LRUPoolEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+        }
+    }
+#endif
 }
