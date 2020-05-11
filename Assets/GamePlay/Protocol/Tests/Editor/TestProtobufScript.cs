b0a838a2ede1842b26b827abe210282dedce40f9
xukun <xukunn1226@gmail.com>
2020-05-11 18:42:41 +0800

    66

--- Assets/GamePlay/Protocol/Tests/Editor/TestProtobufScript.cs
+++ Assets/GamePlay/Protocol/Tests/Editor/TestProtobufScript.cs
@@ -26,7 +26,7 @@ namespace Tests
             toServer.Dic.Add("b", 2);
             toServer.Dic.Add("aa", 1);
             byte[] data = toServer.ToByteArray();
-            using (var output = System.IO.File.Create("assets/gameplay/tests/editor/test_proto.bytes"))
+            using (var output = System.IO.File.Create("assets/gameplay/protocol/tests/editor/test_proto.bytes"))
             {
                 toServer.WriteTo(output);
             }
@@ -38,7 +38,7 @@ namespace Tests
             StoreRequest toClient = new StoreRequest();
             toClient = StoreRequest.Parser.ParseFrom(data);
 
-            using (var input = System.IO.File.OpenRead("assets/gameplay/tests/editor/test_proto.bytes"))
+            using (var input = System.IO.File.OpenRead("assets/gameplay/protocol/tests/editor/test_proto.bytes"))
             {
                 StoreRequest client2 = StoreRequest.Parser.ParseFrom(input);
             }
