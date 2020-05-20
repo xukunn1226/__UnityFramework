b4079f85e78ca590bb8b05d2e05f7b4830d44386
xukun <xukunn1226@gmail.com>
2020-05-20 19:06:26 +0800

    11

--- Assets/Framework/AssetManagement/AssetBuilder/Editor/AssetPostprocessor/ModelPostprocessor.cs
+++ Assets/Framework/AssetManagement/AssetBuilder/Editor/AssetPostprocessor/ModelPostprocessor.cs
@@ -5,33 +5,33 @@ namespace Framework.AssetManagement.AssetBuilder
 {
     public class ModelPostprocessor : AssetPostprocessor
     {
-        void OnPreprocessModel()
-        {
-            ModelImporter modelImporter = assetImporter as ModelImporter;
-#if UNITY_2019_1_OR_NEWER
-            modelImporter.materialImportMode = ModelImporterMaterialImportMode.None;
-#else
-            modelImporter.importMaterials = false;
-#endif
-        }
+//        void OnPreprocessModel()
+//        {
+//            ModelImporter modelImporter = assetImporter as ModelImporter;
+//#if UNITY_2019_1_OR_NEWER
+//            modelImporter.materialImportMode = ModelImporterMaterialImportMode.None;
+//#else
+//            modelImporter.importMaterials = false;
+//#endif
+//        }
 
-        void OnPostprocessModel(GameObject root)
-        {
-            ModelImporter modelImporter = assetImporter as ModelImporter;
-#if UNITY_2019_1_OR_NEWER
-            if(modelImporter.materialImportMode == ModelImporterMaterialImportMode.None)
-#else
-            if (!modelImporter.importMaterials)
-#endif
-            {
-                // 清空默认的内置引用资源
-                Renderer[] rdrs = root.GetComponentsInChildren<Renderer>();
-                for (int i = 0; i < rdrs.Length; ++i)
-                {
-                    Material[] mats = new Material[rdrs[i].sharedMaterials.Length];
-                    rdrs[i].sharedMaterials = mats;
-                }
-            }
-        }
+//        void OnPostprocessModel(GameObject root)
+//        {
+//            ModelImporter modelImporter = assetImporter as ModelImporter;
+//#if UNITY_2019_1_OR_NEWER
+//            if(modelImporter.materialImportMode == ModelImporterMaterialImportMode.None)
+//#else
+//            if (!modelImporter.importMaterials)
+//#endif
+//            {
+//                // 清空默认的内置引用资源
+//                Renderer[] rdrs = root.GetComponentsInChildren<Renderer>();
+//                for (int i = 0; i < rdrs.Length; ++i)
+//                {
+//                    Material[] mats = new Material[rdrs[i].sharedMaterials.Length];
+//                    rdrs[i].sharedMaterials = mats;
+//                }
+//            }
+//        }
     }
 }
