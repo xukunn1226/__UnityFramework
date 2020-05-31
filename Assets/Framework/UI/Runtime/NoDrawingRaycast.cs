cad5ce2e82eeb92ff5ec20e952822bda0d18d69e
xukun <xukunn1226@gmail.com>
2020-06-01 00:28:36 +0800

    NoDrawingRaycast..cs

---
+++ Assets/Framework/UI/Runtime/NoDrawingRaycast.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Framework.UI.Runtime
+{
+    public class NoDrawingRaycast : Graphic
+    {
+        public override void SetMaterialDirty()
+        {}
+
+        public override void SetVerticesDirty()
+        {}
+
+        protected override void OnPopulateMesh(VertexHelper vh)
+        {
+            base.OnPopulateMesh(vh);
+        }
+    }
+}
