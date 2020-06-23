db7f32c23b97781ca3e65d17f40a48b550f67b2d
xukun <xukunn1226@gmail.com>
2020-06-23 17:46:41 +0800

    暂时关闭SRP; ShowOverdraw统计填充率

---
+++ Assets/Application/MeshParticleSystem/ParticleProfiler/Editor/ShowOverdrawEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ShowOverdraw))]
+public class ShowOverdrawEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        ShowOverdraw obj = (ShowOverdraw)target;
+
+        EditorGUI.BeginDisabledGroup(true);
+        EditorGUILayout.ObjectField("RenderTexture", obj.m_RT, typeof(RenderTexture), false);
+        
+        EditorGUILayout.LabelField("Frame Count", obj.m_FrameCount.ToString());
+        EditorGUILayout.LabelField("Pixel Total", obj.GetAveragePixDraw().ToString());
+        EditorGUILayout.LabelField("Actual Pixel Total", obj.GetAverageActualPixDraw().ToString());
+        EditorGUILayout.LabelField("Fill Rate", obj.GetAverageFillrate().ToString());
+        
+        EditorGUI.EndDisabledGroup();
+    }
+}
