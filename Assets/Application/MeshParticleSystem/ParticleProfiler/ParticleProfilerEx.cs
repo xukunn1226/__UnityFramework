dc6707cfe6dc3cbe1236180af0ef87ab42710f02
xukun <xukunn1226@gmail.com>
2020-06-23 19:04:01 +0800

    .

---
+++ Assets/Application/MeshParticleSystem/ParticleProfiler/ParticleProfilerEx.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace MeshParticleSystem.Profiler
+{
+    public class ParticleProfilerEx : MonoBehaviour
+    {
+        public List<ParticleSystem> allParticles            = new List<ParticleSystem>();
+        public List<FX_Component>   allFXComponents         = new List<FX_Component>();
+        public List<Texture>        allTextures             = new List<Texture>();
+        public List<Material>       allMaterials            = new List<Material>();
+        public List<Mesh>           allMeshes               = new List<Mesh>();
+        
+        public int                  materialCount           { get; set; }
+        public long                 textureMemory           { get; set; }
+        public long                 textureMemoryOnAndroid  { get; set; }
+        public long                 textureMemoryOnIPhone   { get; set; }
+        public int                  maxDrawCall             { get; set; }
+        private int                 m_CurDrawCall;
+        public int                  curDrawCall             { get { return m_CurDrawCall; } set { if(value > maxDrawCall) maxDrawCall = value; m_CurDrawCall = value; } }
+
+        public int                  maxTriangles            { get; set; }
+        private int                 m_CurTriangles;
+        public int                  curTriangles            { get { return m_CurTriangles; } set { if(value > maxTriangles) maxTriangles = value; m_CurTriangles = value; } }
+
+        public int                  maxParticleCount        { get; set; }
+        private int                 m_CurParticleCount;
+        public int                  curParticleCount        { get { return m_CurParticleCount; } set { if(value > maxParticleCount) maxParticleCount = value; m_CurParticleCount = value; } }
+
+        public AnimationCurve       DrawCallCurve           = new AnimationCurve();
+        public AnimationCurve       TriangleCountCurve      = new AnimationCurve();
+        public AnimationCurve       ParticleCountCurve      = new AnimationCurve();
+
+        void OnEnable()
+        {
+            allParticles            = GetComponentsInChildren<ParticleSystem>(true).ToList();
+            allFXComponents         = GetComponentsInChildren<FX_Component>(true).ToList();
+            allMeshes               = Utility.GetAllMeshes(gameObject);
+            allMaterials            = Utility.GetAllMaterials(gameObject);
+            allTextures             = Utility.GetAllTextures(gameObject);
+            materialCount           = allMaterials.Count;
+            textureMemory           = Utility.GetRuntimeMemorySizeLong(allTextures);
+            textureMemoryOnAndroid  = Utility.GetRuntimeMemorySizeLongOnAndroid(allTextures);
+            textureMemoryOnIPhone   = Utility.GetRuntimeMemorySizeLongOnIPhone(allTextures);
+        }
+
+        void Update()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Application/MeshParticleSystem/ParticleProfiler/Utility.cs
@@ -0,0 +1,250 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Reflection;
+using System;
+using System.Linq;
+
+namespace MeshParticleSystem.Profiler
+{
+    static public class Utility
+    {
+        static public MethodInfo m_GetRuntimeMemorySizeLong = null;                 // 运行时纹理内存大小
+        static public MethodInfo m_GetTextureFormat = null;                         // 当前平台纹理格式
+
+        static public long GetRuntimeMemorySizeLong(Texture texture)
+        {
+            if(m_GetRuntimeMemorySizeLong == null)
+            {
+                m_GetRuntimeMemorySizeLong = typeof(AssetDatabase).Assembly.GetType("UnityEditor.TextureUtil").GetMethod("GetRuntimeMemorySizeLong", BindingFlags.Public | BindingFlags.Static);
+            }
+            return (long)(m_GetRuntimeMemorySizeLong?.Invoke(null, new object[] {texture}) ?? 0);
+        }
+
+        static public string GetTextureFormatString(Texture texture)
+        {
+            if(m_GetTextureFormat == null)
+            {
+                m_GetTextureFormat = typeof(AssetDatabase).Assembly.GetType("UnityEditor.TextureUtil").GetMethod("GetTextureFormat", BindingFlags.Public | BindingFlags.Static);
+            }
+            return m_GetTextureFormat?.Invoke(null, new object[] {texture}).ToString() ?? null;
+        }
+
+        static public long GetRuntimeMemorySizeLongOnAndroid(Texture texture)
+        {
+            string path = AssetDatabase.GetAssetPath(texture);
+            TextureImporter ti = TextureImporter.GetAtPath(path) as TextureImporter;
+            if(ti == null)
+                return 0;
+
+            long size = texture.width * texture.height * (ti.DoesSourceTextureHaveAlpha() ? 8 : 4) / 8;
+            if(ti.mipmapEnabled)
+                size = size * 13 / 10;
+
+            return size;
+        }
+
+        static public long GetRuntimeMemorySizeLongOnIPhone(Texture texture)
+        {
+            string path = AssetDatabase.GetAssetPath(texture);
+            TextureImporter ti = TextureImporter.GetAtPath(path) as TextureImporter;
+            if(ti == null)
+                return 0;
+
+            float size = texture.width * texture.height * (3.56f) / 8;
+            if(texture.mipmapCount > 0)
+                size = size * 1.3f;
+
+            return (long)size;
+        }
+        
+        static private int GetTotalParticleCount(List<ParticleSystem> psList)
+        {
+            int count = 0;
+            foreach(var ps in psList)
+            {
+                count += ps.particleCount;
+            }
+            return count;
+        }
+
+        static public long GetRuntimeMemorySizeLong(List<Texture> texList)
+        {
+            long size = 0;
+            foreach(var tex in texList)
+            {
+                size += GetRuntimeMemorySizeLong(tex);
+            }
+            return size;
+        }
+
+        static public long GetRuntimeMemorySizeLongOnAndroid(List<Texture> texList)
+        {
+            long size = 0;
+            foreach(var tex in texList)
+            {
+                size += GetRuntimeMemorySizeLongOnAndroid(tex);
+            }
+            return size;
+        }
+
+        static public long GetRuntimeMemorySizeLongOnIPhone(List<Texture> texList)
+        {
+            long size = 0;
+            foreach(var tex in texList)
+            {
+                size += GetRuntimeMemorySizeLongOnIPhone(tex);
+            }
+            return size;
+        }
+
+        static public List<Mesh> GetAllMeshes(GameObject particle)
+        {
+            if(particle == null)
+                throw new System.ArgumentNullException("particle");
+
+            HashSet<Mesh> meshSet = new HashSet<Mesh>();
+
+            MeshFilter[] mfs = particle.GetComponentsInChildren<MeshFilter>(true);
+            foreach(var mf in mfs)
+            {
+                if(mf == null || mf.sharedMesh == null) continue;
+                meshSet.Add(mf.sharedMesh);
+            }
+
+            SkinnedMeshRenderer[] smrs = particle.GetComponentsInChildren<SkinnedMeshRenderer>(true);
+            foreach(var smr in smrs)
+            {
+                if(smr == null || smr.sharedMesh == null) continue;
+                meshSet.Add(smr.sharedMesh);
+            }
+
+            ParticleSystem[] pss = particle.GetComponentsInChildren<ParticleSystem>(true);
+            foreach(var ps in pss)
+            {
+                var psr = ps.GetComponent<ParticleSystemRenderer>();
+                if(psr.renderMode == ParticleSystemRenderMode.Mesh && psr.mesh != null)
+                {
+                    meshSet.Add(psr.mesh);
+                }
+            }
+            return meshSet.ToList();
+        }
+
+        static public List<Texture> GetAllTextures(GameObject particle)
+        {
+            if(particle == null)
+                throw new System.ArgumentNullException("particle");
+
+            HashSet<Texture> texSet = new HashSet<Texture>();
+
+            texSet.UnionWith(GetAllTextureSheet(particle));
+
+            List<Material> matList = GetAllMaterials(particle);
+            foreach(var mat in matList)
+            {
+                int count = ShaderUtil.GetPropertyCount(mat.shader);
+				for (int i = 0; i < count; i++)
+				{
+					ShaderUtil.ShaderPropertyType propertyType = ShaderUtil.GetPropertyType(mat.shader, i);
+					if (propertyType == ShaderUtil.ShaderPropertyType.TexEnv)
+					{
+						string propertyName = ShaderUtil.GetPropertyName(mat.shader, i);
+						Texture tex = mat.GetTexture(propertyName);
+						if (tex != null)
+						{
+                            texSet.Add(tex);
+						}
+					}
+				}
+            }
+            return texSet.ToList();
+        }
+
+        static public HashSet<Texture> GetAllTextureSheet(GameObject particle)
+        {
+            HashSet<Texture> texSet = new HashSet<Texture>();
+
+            ParticleSystem[] pss = particle.GetComponentsInChildren<ParticleSystem>(true);
+            foreach(var ps in pss)
+            {
+                ParticleSystem.TextureSheetAnimationModule tsa = ps.textureSheetAnimation;
+                if(tsa.mode == ParticleSystemAnimationMode.Sprites)
+                {
+                    for(int i = 0; i < tsa.spriteCount; ++i)
+                    {
+                        Sprite s = tsa.GetSprite(i);
+                        if(s != null && s.texture != null)
+                        {
+                            texSet.Add(s.texture);
+                        }
+                    }
+                }
+            }
+
+            return texSet;
+        }
+
+        static public List<Material> GetAllMaterials(GameObject particle)
+        {
+            if(particle == null)
+                throw new System.ArgumentNullException("particle");
+
+            HashSet<Material> matSet = new HashSet<Material>();
+
+            ParticleSystem[] pss = particle.GetComponentsInChildren<ParticleSystem>(true);
+            foreach(var ps in pss)
+            {
+                matSet.UnionWith(GetMaterials(ps));
+            }
+
+            FX_Component[] fxs = particle.GetComponentsInChildren<FX_Component>(true);
+            foreach(var fx in fxs)
+            {
+                matSet.UnionWith(GetMaterials(fx));
+            }
+            return matSet.ToList();
+        }
+
+        static public HashSet<Material> GetMaterials(ParticleSystem ps)
+        {
+            HashSet<Material> matSet = new HashSet<Material>();
+
+            ParticleSystemRenderer[] psrs = ps.GetComponentsInChildren<ParticleSystemRenderer>(true);
+            foreach(var psr in psrs)
+            {
+                if(psr == null) continue;
+
+                foreach(var mat in psr.sharedMaterials)
+                {
+                    if(mat == null) continue;
+                    matSet.Add(mat);
+                }
+
+                if(psr.trailMaterial != null)
+                    matSet.Add(psr.trailMaterial);
+            }
+            return matSet;
+        }
+
+        static public HashSet<Material> GetMaterials(FX_Component fxComp)
+        {
+            HashSet<Material> matSet = new HashSet<Material>();
+
+            Renderer[] rdrs = fxComp.GetComponents<Renderer>();
+            foreach(var rdr in rdrs)
+            {
+                if(rdr == null) continue;
+
+                foreach(var mat in rdr.sharedMaterials)
+                {
+                    if(mat == null) continue;
+                    matSet.Add(mat);
+                }
+            }
+
+            return matSet;
+        }
+    }
+}
