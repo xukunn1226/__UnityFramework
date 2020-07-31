82f4701d3d3b909c1885d2c9bc9632059cd402ff
xukun <xukunn1226@gmail.com>
2020-07-31 14:27:40 +0800

    .

--- Assets/Framework/AssetManagement/AssetBuilder/Editor/AssetPostprocessor/TexturePostprocessor.cs
+++ Assets/Framework/AssetManagement/AssetBuilder/Editor/AssetPostprocessor/TexturePostprocessor.cs
@@ -1,8 +1,66 @@
 ﻿using UnityEditor;
+using UnityEngine;
+using System.Reflection;
 
 namespace Framework.AssetManagement.AssetBuilder
 {
     public class TexturePostprocessor : AssetPostprocessor
     {
+        void OnPreprocessTexture()
+        {
+            // TextureImporter ti = assetImporter as TextureImporter;
+
+            // // 设置平台无关属性
+            // TextureImporterSettings importerSettings = new TextureImporterSettings();
+            // ti.ReadTextureSettings(importerSettings);
+            // // importerSettings.sRGBTexture = true;
+            // // importerSettings.mipmapEnabled = true;
+            // ti.SetTextureSettings(importerSettings);
+
+            // // int width, height;
+            // // GetTextureOriginalSize(ti, out width, out height);
+
+            // // standalone
+            // TextureImporterPlatformSettings standalone_platformSetting = ti.GetPlatformTextureSettings("Standalone");
+            // standalone_platformSetting.overridden = true;
+            // standalone_platformSetting.allowsAlphaSplitting = false;
+            // standalone_platformSetting.compressionQuality = 50;
+            // // standalone_platformSetting.maxTextureSize = width >> 1;
+            // ti.SetPlatformTextureSettings(standalone_platformSetting);
+
+            // // android
+            // TextureImporterPlatformSettings android_platformSetting = ti.GetPlatformTextureSettings("Android");
+            // android_platformSetting.overridden = true;
+            // android_platformSetting.allowsAlphaSplitting = false;
+            // android_platformSetting.compressionQuality = 50;
+            // // android_platformSetting.maxTextureSize = maxTextureSize;
+            // ti.SetPlatformTextureSettings(android_platformSetting);
+
+            // // ios
+            // TextureImporterPlatformSettings ios_platformSetting = ti.GetPlatformTextureSettings("iPhone");
+            // ios_platformSetting.overridden = true;
+            // ios_platformSetting.allowsAlphaSplitting = false;
+            // ios_platformSetting.compressionQuality = 50;
+            // // ios_platformSetting.maxTextureSize = maxTextureSize;
+            // ti.SetPlatformTextureSettings(ios_platformSetting);
+        }
+
+        private static void GetTextureOriginalSize(TextureImporter ti, out int width, out int height)
+        {
+            if (ti == null)
+            {
+                width = 0;
+                height = 0;
+                return;
+            }
+
+            object[] args = new object[2] { 0, 0 };
+            MethodInfo mi = typeof(TextureImporter).GetMethod("GetWidthAndHeight", BindingFlags.NonPublic | BindingFlags.Instance);
+            mi.Invoke(ti, args);
+
+            width = (int)args[0];
+            height = (int)args[1];
+        }
     }
 }
+
