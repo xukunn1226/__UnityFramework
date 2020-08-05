cd404aa045dc32b0c1f73df45b8de6e33e215b34
xukun <xukunn1226@gmail.com>
2020-08-05 19:06:06 +0800

    .

---
+++ Assets/Framework/AssetManagement/GameBuilder/Editor/NewBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Build.Player;
+
+public class NewBuilder
+{
+    // Start is called before the first frame update
+    static public AssetBundleManifest BuildAssetBundles()
+    {
+        return null;
+    }
+}
