bc1960d6756531af67a3368092c84e1f8e64eb73
xukun <xukunn1226@gmail.com>
2020-08-18 00:55:37 +0800

    。

---
+++ Assets/Framework/Core/Attribute/PopupAttribute.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Framework.Core
+{
+    public class PopupAttribute : PropertyAttribute
+    {
+        public object[] dataSet;
+
+        public Type dataType;
+        
+        public PopupAttribute(Type dataType, params object[] list)
+        {
+            this.dataType = dataType;
+            this.dataSet = list;
+        }
+    }
+    
+    [CustomPropertyDrawer(typeof(PopupAttribute))]
+    internal sealed class PopupDrawer : PropertyDrawer
+    {
+        List<string> displayNames = new List<string>();
+        int selected;
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            PopupAttribute pop = (PopupAttribute)attribute;
+            displayNames.Clear();
+            for (int i = 0; i < pop.dataSet.Length; i++)
+            {
+                object data = pop.dataSet[i];
+                displayNames.Add(data.ToString());
+                if (pop.dataType == typeof(int) && property.intValue == (int)data)
+                {
+                    selected = i;
+                }
+                else
+                {
+                    //....
+                }
+            }
+
+            EditorGUI.BeginChangeCheck();
+            selected = EditorGUI.Popup(position, label.text, selected, displayNames.ToArray());
+            if (EditorGUI.EndChangeCheck())
+            {
+                if (pop.dataType == typeof(int))
+                {
+                    property.intValue = (int)pop.dataSet[selected];
+                }
+                else
+                {
+                    //....
+                }
+                property.serializedObject.ApplyModifiedProperties();
+            }
+        }
+    }
+}
