6cc571d64bbf72223bf644926bd1e9a6ade900cb
xukun <xukunn1226@gmail.com>
2020-08-28 16:08:09 +0800

    .

---
+++ Assets/Framework/Core/Utility/Runtime/ListPool.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework.Core
+{
+    public static class ListPool<T>
+    {
+        // Object pool to avoid allocations.
+        private static readonly SimpleObjectPool<List<T>> s_ListPool = new SimpleObjectPool<List<T>>(null, Clear);
+        static void Clear(List<T> l) { l.Clear(); }
+
+        public static List<T> Get()
+        {
+            return s_ListPool.Get();
+        }
+
+        public static void Release(List<T> toRelease)
+        {
+            s_ListPool.Release(toRelease);
+        }
+    }
+}
