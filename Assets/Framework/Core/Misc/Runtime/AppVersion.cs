5a583db1aa8232c670956f584826ed63cad9218e
xukun <xukunn1226@gmail.com>
2020-11-01 03:30:00 +0800

    .

--- Assets/Framework/Core/Misc/Runtime/AppVersion.cs
+++ Assets/Framework/Core/Misc/Runtime/AppVersion.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 namespace Framework.Core
 {
-    public class AppVersion : ScriptableObject
+    public class AppVersion : ScriptableObject, IComparable<AppVersion>
     {
         public int MainVersion;
-        public int SecondaryVersion;
         public int MinorVersion;
+        public int Revision;
         public int BuildNumber;
+
+        new public string ToString()
+        {
+            return string.Format($"{MainVersion}.{MinorVersion}.{Revision}");
+        }
+
+        public int CompareTo(AppVersion other)
+        {
+            return 0;
+        }
     }
 }
