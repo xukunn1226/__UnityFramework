743b2574d7fe8053bf0e05b3426ee5329cb81fca
xukun <xukunn1226@gmail.com>
2020-12-09 18:51:22 +0800

    2

--- BuildScript/Deploy.bat
+++ BuildScript/Deploy.bat
@@ -15,14 +15,13 @@ set BACKUP_ROOTPATH_PARAMETER=Deployment
 
 :: Backup AppDirectory
 set BACKUP_APPDIRECTORY=-AppDirectory
-set BACKUP_APPDIRECTORY_PARAMETER=0.12.3
+set BACKUP_APPDIRECTORY_PARAMETER=0.0.4
 
 
 echo %BATCH% %QUIT% -executeMethod Framework.AssetManagement.GameBuilder.Deployment.cmdDeploy  %BACKUP_ROOTPATH% %BACKUP_ROOTPATH_PARAMETER% %BACKUP_APPDIRECTORY% %BACKUP_APPDIRECTORY_PARAMETER%
-"D:\Program Files\2020.2.0b5\Editor\Unity.exe" %BATCH% %QUIT% %PROJECT% %PROJECT_PATH% -executeMethod Framework.AssetManagement.GameBuilder.Deployment.cmdDeploy  %BACKUP_ROOTPATH% %BACKUP_ROOTPATH_PARAMETER%
+"D:\Program Files\2020.2.0b5\Editor\Unity.exe" %BATCH% %QUIT% %PROJECT% %PROJECT_PATH% -executeMethod Framework.AssetManagement.GameBuilder.Deployment.cmdDeploy  %BACKUP_ROOTPATH% %BACKUP_ROOTPATH_PARAMETER% %BACKUP_APPDIRECTORY% %BACKUP_APPDIRECTORY_PARAMETER%
 
-:: %BACKUP_APPDIRECTORY% %BACKUP_APPDIRECTORY_PARAMETER%
-
+echo %errorlevel%
 
 echo End Backup.....
 
