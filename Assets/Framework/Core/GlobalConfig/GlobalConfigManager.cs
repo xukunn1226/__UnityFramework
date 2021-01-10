481831a9c7f08b064bf786c94dc6f0a2f15195a3
xukun <xukunn1226@gmail.com>
2021-01-11 00:27:55 +0800

    。

--- Assets/Framework/Core/GlobalConfig/GlobalConfigManager.cs
+++ Assets/Framework/Core/GlobalConfig/GlobalConfigManager.cs
@@ -2,16 +2,72 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.IO;
 using System.Reflection;
 using Newtonsoft.Json;
+using Framework.AssetManagement.Runtime;
 
 namespace Framework.Core
 {
     static public class GlobalConfigManager
     {
-        static private string s_ConfigPath = "assets/res/globalconfig";
+        static private string                           s_ConfigPath        = "assets/res/globalconfig";
+        static private Dictionary<string, GlobalConfig> s_DefaultConfigs    = new Dictionary<string, GlobalConfig>();
+        static private Dictionary<string, GlobalConfig> s_Configs           = new Dictionary<string, GlobalConfig>();
 
-        static private Dictionary<string, GlobalConfig> s_ConfigDic = new Dictionary<string, GlobalConfig>();
+        static private AssetBundleLoader m_BundleLoader;
+
+        static public void Init(bool InEditorMode = true)
+        {
+#if UNITY_EDITOR
+#else
+            InEditorMode = false;
+#endif
+            if (InEditorMode)
+            { // from assetdatabase
+#if UNITY_EDITOR
+                DirectoryInfo di = new DirectoryInfo(s_ConfigPath);
+                FileInfo[] fis = di.GetFiles("*.bytes", SearchOption.AllDirectories);
+                byte[] array = new byte[1024 * 128];
+                foreach (var fi in fis)
+                {
+                    FileStream fs = new FileStream(fi.FullName, FileMode.Open);
+                    int size = fs.Read(array, 0, 1024 * 128);
+                    fs.Close();
+                    string json = System.Text.Encoding.UTF8.GetString(array, 0, size);
+                    GlobalConfig config = JsonConvert.DeserializeObject<GlobalConfig>(json);
+
+                    s_DefaultConfigs.Add(fi.Name, config);
+                }
+#endif
+            }
+            else
+            { // from asset bundle
+                m_BundleLoader = AssetManager.LoadAssetBundle(s_ConfigPath + ".ab");
+                TextAsset[] tas = m_BundleLoader.assetBundle.LoadAllAssets<TextAsset>();
+            }
+
+            BuildSavedConfigs();
+        }
+
+        static private void BuildSavedConfigs()
+        {
+            foreach(var defaultConfig in s_DefaultConfigs)
+            {
+
+            }
+        }
+
+        static public void Uninit()
+        {
+            if(m_BundleLoader != null)
+            {
+                AssetManager.UnloadAssetBundle(m_BundleLoader);
+                m_BundleLoader = null;
+            }
+            s_DefaultConfigs.Clear();
+            s_Configs.Clear();
+        }
 
         static public void LoadConfig(this IConfig instance)
         {
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Framework.Core;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GlobalConfigManager.Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
