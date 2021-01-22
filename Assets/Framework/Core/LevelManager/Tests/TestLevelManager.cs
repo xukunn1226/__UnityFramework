5bee96973b5f7b0fcd8d6430d98745a1ec7236a3
xukun <xukunn1226@gmail.com>
2021-01-22 16:47:59 +0800

    1

--- Assets/Framework/Core/LevelManager/Tests/TestLevelManager.cs
+++ Assets/Framework/Core/LevelManager/Tests/TestLevelManager.cs
@@ -4,7 +4,7 @@ namespace Framework.LevelManager.Tests
 {
     public class TestLevelManager : MonoBehaviour
     {
-        private const string m_SceneBundlePath = "assets/framework/levelmanager/tests/res/scenes.ab";
+        private const string m_SceneBundlePath = "assets/framework/core/levelmanager/tests/res/scenes.ab";
         
         private void Start()
         {
@@ -13,14 +13,14 @@ namespace Framework.LevelManager.Tests
             LevelManager.levelCommandBegin += LevelCommandBegin;
             LevelManager.levelCommandEnd += LevelCommandEnd;
 
-            LevelManager.LoadLevelContext ctx = new LevelManager.LoadLevelContext();
-            ctx.identifier = "tscene1";
-            ctx.fromBundle = true;
-            ctx.levelPath = "Assets/Framework/LevelManager/Tests/Res/Scenes/TestScene1.unity";
-            ctx.additive = true;
-            ctx.bundlePath = m_SceneBundlePath;
+            // LevelManager.LoadLevelContext ctx = new LevelManager.LoadLevelContext();
+            // ctx.identifier = "tscene1";
+            // ctx.fromBundle = true;
+            // ctx.levelPath = "Assets/Framework/LevelManager/Tests/Res/Scenes/TestScene1.unity";
+            // ctx.additive = true;
+            // ctx.bundlePath = m_SceneBundlePath;
 
-            LevelManager.Instance.LoadAsync(ctx);
+            // LevelManager.Instance.LoadAsync(ctx);
         }
 
         private void LevelCommandBegin(string identifier, bool isLoaded)
@@ -35,36 +35,37 @@ namespace Framework.LevelManager.Tests
 
         private void OnGUI()
         {
-            if (GUI.Button(new Rect(100, 100, 150, 60), "Load Scene2"))
+            if (GUI.Button(new Rect(100, 100, 300, 120), "Load Scene2"))
             {
                 LevelManager.LoadLevelContext ctx = new LevelManager.LoadLevelContext();
                 ctx.identifier = "tscene2";
                 ctx.fromBundle = true;
-                ctx.levelPath = "Assets/Framework/LevelManager/Tests/Res/Scenes/TestScene2.unity";
+                ctx.levelPath = "Assets/Framework/Core/LevelManager/Tests/Res/Scenes/TestScene2.unity";
+                // ctx.levelPath = "TestScene2";
                 ctx.additive = true;
                 ctx.bundlePath = m_SceneBundlePath;
 
                 LevelManager.Instance.LoadAsync(ctx);
             }
 
-            if (GUI.Button(new Rect(300, 100, 150, 60), "Unload Scene2"))
+            if (GUI.Button(new Rect(500, 100, 300, 120), "Unload Scene2"))
             {
                 LevelManager.Instance.UnloadLevelAsync("tscene2");
             }
 
-            if (GUI.Button(new Rect(100, 200, 150, 60), "Load Scene3"))
+            if (GUI.Button(new Rect(100, 300, 300, 120), "Load Scene3"))
             {
                 LevelManager.LoadLevelContext ctx = new LevelManager.LoadLevelContext();
                 ctx.identifier = "tscene3";
                 ctx.fromBundle = true;
-                ctx.levelPath = "Assets/Framework/LevelManager/Tests/Res/Scenes/TestScene3.unity";
+                ctx.levelPath = "Assets/Framework/Core/LevelManager/Tests/Res/Scenes/TestScene3.unity";
                 ctx.additive = false;
                 ctx.bundlePath = m_SceneBundlePath;
 
                 LevelManager.Instance.LoadAsync(ctx);
             }
 
-            if (GUI.Button(new Rect(300, 200, 150, 60), "Unload Scene3"))
+            if (GUI.Button(new Rect(500, 300, 300, 120), "Unload Scene3"))
             {
                 LevelManager.Instance.UnloadLevelAsync("tscene3");
             }
