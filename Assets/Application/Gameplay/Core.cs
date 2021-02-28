9927dae43f51d11ca55368097acbf76e2715608b
xukun <xukunn1226@gmail.com>
2021-03-01 02:08:40 +0800

    。

--- Assets/Application/Gameplay/Core.cs
+++ Assets/Application/Gameplay/Core.cs
@@ -9,31 +9,11 @@ namespace Application.Runtime
     /// <summary>
     /// 负责初始核心组件，然后加载游戏场景
     /// <summary>
-    public class Core : MonoBehaviour
+    public class Core : SingletonMono<Core>
     {
         public string   TheFirstGameSceneName;
         public string   ScenePath;
         public string   BundlePath;
-
-        static public Core Instance
-        {
-            get; private set;
-        }
-
-        private void Awake()
-        {
-            if (FindObjectsOfType<Core>().Length > 1)
-            {
-                DestroyImmediate(this);
-                throw new Exception("Core has already exist...");
-            }
-
-            gameObject.name = "[Core]";
-
-            Instance = this;
-
-            DontDestroyOnLoad(gameObject);
-        }
 
         IEnumerator Start()
         {
