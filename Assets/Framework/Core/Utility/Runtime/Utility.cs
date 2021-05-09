816058d234af403a15370217603773b5f60e9015
xukun <xukunn1226@gmail.com>
2021-05-09 19:01:58 +0800

    。

--- Assets/Framework/Core/Utility/Runtime/Utility.cs
+++ Assets/Framework/Core/Utility/Runtime/Utility.cs
@@ -75,5 +75,33 @@ namespace Framework.Core
             }
             return fullPath;
         }
+
+#if UNITY_ANDROID
+        static public void RestartApp()
+        {
+            using (AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+            {
+                AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+                AndroidJavaObject pm = currentActivity.Call<AndroidJavaObject>("getPackageManager");
+                AndroidJavaObject intent = pm.Call<AndroidJavaObject>("getLaunchIntentForPackage", Application.identifier);
+                intent.Call<AndroidJavaObject>("setFlags", 0x20000000);//Intent.FLAG_ACTIVITY_SINGLE_TOP
+
+                AndroidJavaClass pendingIntent = new AndroidJavaClass("android.app.PendingIntent");
+                AndroidJavaObject contentIntent = pendingIntent.CallStatic<AndroidJavaObject>("getActivity", currentActivity, 0, intent, 0x8000000); //PendingIntent.FLAG_UPDATE_CURRENT = 134217728 [0x8000000]
+                AndroidJavaObject alarmManager = currentActivity.Call<AndroidJavaObject>("getSystemService", "alarm");
+                AndroidJavaClass system = new AndroidJavaClass("java.lang.System");
+                long currentTime = system.CallStatic<long>("currentTimeMillis");
+                alarmManager.Call("set", 1, currentTime + 1000, contentIntent); // android.app.AlarmManager.RTC = 1 [0x1]
+
+                Debug.LogError("alarm_manager set time " + currentTime + 1000);
+                currentActivity.Call("finish");
+
+                AndroidJavaClass process = new AndroidJavaClass("android.os.Process");
+                int pid = process.CallStatic<int>("myPid");
+                process.CallStatic("killProcess", pid);
+            }
+        }
+#endif        
     }
 }
