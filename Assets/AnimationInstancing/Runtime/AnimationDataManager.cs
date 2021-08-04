b109ed5ec072c7e863acd8ec6fc98b47ccf7edf0
xukun <xukunn1226@gmail.com>
2021-08-04 19:07:35 +0800

    add AnimationDataManager

---
+++ Assets/AnimationInstancing/Runtime/AnimationDataManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Framework.Core;
+
+namespace AnimationInstancingModule.Runtime
+{
+    public class AnimationDataManager : SingletonMono<AnimationDataManager>
+    {
+    }
+}
