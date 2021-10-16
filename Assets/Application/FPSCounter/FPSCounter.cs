2b2355276846eaa3c07661871edbd935d44d02b3
xukun <xukunn1226@gmail.com>
2021-10-17 00:01:50 +0800

    ..

--- Assets/Application/FPSCounter/FPSCounter.cs
+++ Assets/Application/FPSCounter/FPSCounter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Framework.Core
+namespace Application.Runtime
 {
     public class FPSCounter : MonoBehaviour
     {
--- Assets/Application/FPSCounter/FPSDisplay.cs
+++ Assets/Application/FPSCounter/FPSDisplay.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Framework.Core
+namespace Application.Runtime
 {
     [RequireComponent(typeof(FPSCounter))]
     public class FPSDisplay : MonoBehaviour
