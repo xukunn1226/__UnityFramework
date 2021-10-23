87ca4d59aa176463e224b5e70e5d12d82a32e998
xukun <xukunn1226@gmail.com>
2021-10-23 22:16:39 +0800

    1

--- Assets/Application/hotfix/SQLite/Editor/CodeGenerator.cs
+++ Assets/Application/hotfix/SQLite/Editor/CodeGenerator.cs
@@ -1,21 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using System.IO;
 
 namespace SQLite.Editor
 {
-    public class CodeGenerator : MonoBehaviour
+    public class CodeGenerator : EditorWindow
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        static private string m_ConfigPath = "Assets/Application/hotfix/SQLite/Editor/";            // 配置表路径
+        static private string m_ScriptOutput = "Assets/Application/hotfix/Config/";                 // 导出的配置表结构脚本目录
+        static private string m_Info;
 
+        [MenuItem("Tools/Config CodeGenerator")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<CodeGenerator>();
+            window.titleContent = new GUIContent("CodeGenerator");
+            window.Show();
+        }
+    
+        private void OnGUI()
+        {
+            m_ConfigPath = EditorGUILayout.TextField("配置文件目录", m_ConfigPath);
+
+            if(GUILayout.Button("Generate..."))
+            {
+                Run();
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        static private void Run()
         {
-
+            // find all csv
+            string[] files = Directory.GetFiles(m_ConfigPath, "*.csv", SearchOption.AllDirectories);
+            foreach(var file in files)
+            {
+                ProcessFile(file);
+            }
         }
-    }
+
+        // supported value type: int, float, bool, string, array<int>, array<float>, array<string>
+        static private void ProcessFile(string file)
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(file);
+                foreach (var l in lines)
+                {
+                    Debug.Log(l);
+                }
+            }
+            catch(System.Exception e)
+            {
+                Debug.LogError(e.Message);
+            }
+        }
+    }    
 }
