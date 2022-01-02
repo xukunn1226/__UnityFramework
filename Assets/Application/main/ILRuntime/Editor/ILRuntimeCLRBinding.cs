e4b496971e48ff07926f4da213775b66bd3203f5
xukun <xukunn1226@gmail.com>
2022-01-02 17:49:55 +0800

    。。

--- Assets/Application/main/ILRuntime/Editor/ILRuntimeCLRBinding.cs
+++ Assets/Application/main/ILRuntime/Editor/ILRuntimeCLRBinding.cs
@@ -12,13 +12,13 @@ namespace Application.Runtime
         {
             //用新的分析热更dll调用引用来生成绑定代码
             ILRuntime.Runtime.Enviorment.AppDomain domain = new ILRuntime.Runtime.Enviorment.AppDomain();
-            using (FileStream fs = new FileStream("Assets/StreamingAssets/HotFix_Project.dll", FileMode.Open, FileAccess.Read))
+            using (FileStream fs = new FileStream("Library/ScriptAssemblies/Application.Logic.dll", FileMode.Open, FileAccess.Read))
             {
                 domain.LoadAssembly(fs);
                 
                 ILHelper.RegisterAdaptor(domain);
                 
-                ILRuntime.Runtime.CLRBinding.BindingCodeGenerator.GenerateBindingCode(domain, "Assets/Mono/ILRuntime/Generate");
+                ILRuntime.Runtime.CLRBinding.BindingCodeGenerator.GenerateBindingCode(domain, "Assets/Application/main/ILRuntime/Generate");
             }
 
             AssetDatabase.Refresh();
@@ -31,10 +31,16 @@ namespace Application.Runtime
         {
             //由于跨域继承特殊性太多，自动生成无法实现完全无副作用生成，所以这里提供的代码自动生成主要是给大家生成个初始模版，简化大家的工作
             //大多数情况直接使用自动生成的模版即可，如果遇到问题可以手动去修改生成后的文件，因此这里需要大家自行处理是否覆盖的问题
-            using (System.IO.StreamWriter sw = new System.IO.StreamWriter("Assets/Mono/ILRuntime/ICriticalNotifyCompletionAdapter.cs"))
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter("Assets/Application/main/ILRuntime/Adaptor/MonoBehaviourAdaptor.cs"))
             {
                 sw.WriteLine(ILRuntime.Runtime.Enviorment.CrossBindingCodeGenerator.GenerateCrossBindingAdapterCode(
-                    typeof (ICriticalNotifyCompletion), "ET"));
+                    typeof (MonoBehaviour), "Application.Runtime"));
+            }
+
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter("Assets/Application/main/ILRuntime/Adaptor/IAsyncStateMachineAdaptor.cs"))
+            {
+                sw.WriteLine(ILRuntime.Runtime.Enviorment.CrossBindingCodeGenerator.GenerateCrossBindingAdapterCode(
+                    typeof (IAsyncStateMachine), "Application.Runtime"));
             }
 
             AssetDatabase.Refresh();
