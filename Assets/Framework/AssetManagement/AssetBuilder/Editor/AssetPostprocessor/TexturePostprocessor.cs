5267ed67913f6aacc1a1f25bf7e9f41e1efbdb7e
xukun <xukunn1226@gmail.com>
2022-01-17 23:32:45 +0800

    。

--- Assets/Framework/AssetManagement/AssetBuilder/Editor/AssetPostprocessor/TexturePostprocessor.cs
+++ Assets/Framework/AssetManagement/AssetBuilder/Editor/AssetPostprocessor/TexturePostprocessor.cs
@@ -6,54 +6,54 @@ namespace Framework.AssetManagement.AssetBuilder
 {
     public class TexturePostprocessor : AssetPostprocessor
     {
-        void OnPreprocessTexture()
-        {
-            if(!AssetBuilderUtil.IsPassByWhiteList(assetPath) || AssetBuilderUtil.IsBlockedByBlackList(assetPath))
-                return;
+        // void OnPreprocessTexture()
+        // {
+        //     if(!AssetBuilderUtil.IsPassByWhiteList(assetPath) || AssetBuilderUtil.IsBlockedByBlackList(assetPath))
+        //         return;
 
-            TextureImporter ti = assetImporter as TextureImporter;
-            // 设置平台无关属性
-            TextureImporterSettings importerSettings = new TextureImporterSettings();
-            ti.ReadTextureSettings(importerSettings);
-            // importerSettings.sRGBTexture = true;
-            // importerSettings.mipmapEnabled = true;
-            ti.SetTextureSettings(importerSettings);
+        //     TextureImporter ti = assetImporter as TextureImporter;
+        //     // 设置平台无关属性
+        //     TextureImporterSettings importerSettings = new TextureImporterSettings();
+        //     ti.ReadTextureSettings(importerSettings);
+        //     // importerSettings.sRGBTexture = true;
+        //     // importerSettings.mipmapEnabled = true;
+        //     ti.SetTextureSettings(importerSettings);
 
-            int width, height;
-            GetTextureOriginalSize(ti, out width, out height);
+        //     int width, height;
+        //     GetTextureOriginalSize(ti, out width, out height);
 
-            bool shouldScale = false;
-            if(Mathf.IsPowerOfTwo(width) && Mathf.IsPowerOfTwo(height))
-            {
-                shouldScale = true;
-            }
+        //     bool shouldScale = false;
+        //     if(Mathf.IsPowerOfTwo(width) && Mathf.IsPowerOfTwo(height))
+        //     {
+        //         shouldScale = true;
+        //     }
 
-            Debug.Log($"width: {width}  height:{height}");
+        //     Debug.Log($"width: {width}  height:{height}");
 
-            // standalone
-            TextureImporterPlatformSettings standalone_platformSetting = ti.GetPlatformTextureSettings("Standalone");
-            standalone_platformSetting.overridden = true;
-            standalone_platformSetting.allowsAlphaSplitting = false;
-            standalone_platformSetting.compressionQuality = 50;
-            standalone_platformSetting.maxTextureSize = shouldScale ? Mathf.Max(width, height) >> 1 : 4096;
-            ti.SetPlatformTextureSettings(standalone_platformSetting);
+        //     // standalone
+        //     TextureImporterPlatformSettings standalone_platformSetting = ti.GetPlatformTextureSettings("Standalone");
+        //     standalone_platformSetting.overridden = true;
+        //     standalone_platformSetting.allowsAlphaSplitting = false;
+        //     standalone_platformSetting.compressionQuality = 50;
+        //     standalone_platformSetting.maxTextureSize = shouldScale ? Mathf.Max(width, height) >> 1 : 4096;
+        //     ti.SetPlatformTextureSettings(standalone_platformSetting);
 
-            // android
-            TextureImporterPlatformSettings android_platformSetting = ti.GetPlatformTextureSettings("Android");
-            android_platformSetting.overridden = true;
-            android_platformSetting.allowsAlphaSplitting = false;
-            android_platformSetting.compressionQuality = 50;
-            android_platformSetting.maxTextureSize = shouldScale ? Mathf.Max(width, height) >> 1 : 4096;
-            ti.SetPlatformTextureSettings(android_platformSetting);
+        //     // android
+        //     TextureImporterPlatformSettings android_platformSetting = ti.GetPlatformTextureSettings("Android");
+        //     android_platformSetting.overridden = true;
+        //     android_platformSetting.allowsAlphaSplitting = false;
+        //     android_platformSetting.compressionQuality = 50;
+        //     android_platformSetting.maxTextureSize = shouldScale ? Mathf.Max(width, height) >> 1 : 4096;
+        //     ti.SetPlatformTextureSettings(android_platformSetting);
 
-            // ios
-            TextureImporterPlatformSettings ios_platformSetting = ti.GetPlatformTextureSettings("iPhone");
-            ios_platformSetting.overridden = true;
-            ios_platformSetting.allowsAlphaSplitting = false;
-            ios_platformSetting.compressionQuality = 50;
-            ios_platformSetting.maxTextureSize = shouldScale ? Mathf.Max(width, height) >> 1 : 4096;
-            ti.SetPlatformTextureSettings(ios_platformSetting);
-        }
+        //     // ios
+        //     TextureImporterPlatformSettings ios_platformSetting = ti.GetPlatformTextureSettings("iPhone");
+        //     ios_platformSetting.overridden = true;
+        //     ios_platformSetting.allowsAlphaSplitting = false;
+        //     ios_platformSetting.compressionQuality = 50;
+        //     ios_platformSetting.maxTextureSize = shouldScale ? Mathf.Max(width, height) >> 1 : 4096;
+        //     ti.SetPlatformTextureSettings(ios_platformSetting);
+        // }
 
         private static void GetTextureOriginalSize(TextureImporter ti, out int width, out int height)
         {
