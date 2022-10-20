2b6af74b4570eae6df5e7802fde6e15c19d5a8b6
xukun <xukunn1226@gmail.com>
2022-10-21 01:15:00 +0800

    【工具】可指定目录下需要提取的动画文件

--- Assets/Application/main/AssetPostprocessor/ExtractAnimWindow.cs
+++ Assets/Application/main/AssetPostprocessor/ExtractAnimWindow.cs
@@ -10,25 +10,36 @@ namespace Application.Editor
         static private string s_DstDirectoryKey = "Cache_Dst_Directory_3dcadcdfe3454dccgfd43";
         public string m_SrcDirectory;
         public string m_DstDirectory;
+        public string[] m_PendingFbxs;
+        public List<bool> m_PendingFbxsSelected;
+        private Vector2 m_ScrollPosition;
 
         static public void Init(string srcDirectory)
         {
             ExtractAnimWindow window = GetWindow<ExtractAnimWindow>();
             window.Show();
             window.titleContent = new GUIContent("ExtractAnim Window", EditorGUIUtility.FindTexture("SettingsIcon"));
-            window.position = new Rect(600, 120, 500, 350);
+            window.position = new Rect(600, 120, 800, 600);
             window.m_SrcDirectory = string.IsNullOrEmpty(srcDirectory) ? "Assets/" : srcDirectory;
 
-            string cachedDstDirectory = PlayerPrefs.GetString(s_DstDirectoryKey);
-            window.m_DstDirectory = string.IsNullOrEmpty(cachedDstDirectory) ? srcDirectory : cachedDstDirectory;
+            string cachedDstDirectory = PlayerPrefs.GetString(s_DstDirectoryKey);            
+            window.m_DstDirectory = (string.IsNullOrEmpty(cachedDstDirectory) || !AssetDatabase.IsValidFolder(cachedDstDirectory)) ? srcDirectory : cachedDstDirectory;
+
+            window.m_PendingFbxs = AssetDatabase.FindAssets("t:model", new string[] { window.m_SrcDirectory });
+            window.m_PendingFbxsSelected = new List<bool>(window.m_PendingFbxs.Length);
+            for(int i = 0; i < window.m_PendingFbxs.Length; i++)
+            {
+                window.m_PendingFbxsSelected.Add(true);
+            }
         }
 
         private void OnGUI()
         {
-            EditorGUILayout.Space();
+            m_ScrollPosition = EditorGUILayout.BeginScrollView(m_ScrollPosition);
+            EditorGUILayout.Space();            
             EditorGUILayout.BeginHorizontal();
             GUILayout.Label("当前目录：", EditorStyles.label);
-            if(GUILayout.Button(m_SrcDirectory, GUILayout.Width(400)))
+            if(GUILayout.Button(m_SrcDirectory, GUILayout.Width(600)))
             {
                 string srcDirectory = EditorUtility.OpenFolderPanel("选择当前目录", m_SrcDirectory, m_SrcDirectory);
                 if(!string.IsNullOrEmpty(srcDirectory))
@@ -43,10 +54,16 @@ namespace Application.Editor
             }
             EditorGUILayout.EndHorizontal();
 
+            // display all clips in current folder
+            for(int i = 0; i < m_PendingFbxs.Length; ++i)
+            {
+                m_PendingFbxsSelected[i] = EditorGUILayout.ToggleLeft(System.IO.Path.GetFileNameWithoutExtension(AssetDatabase.GUIDToAssetPath(m_PendingFbxs[i])), m_PendingFbxsSelected[i]);
+            }
+
             EditorGUILayout.Space();
             EditorGUILayout.BeginHorizontal();
             GUILayout.Label("目标目录：", EditorStyles.label);
-            if(GUILayout.Button(m_DstDirectory, GUILayout.Width(400)))
+            if(GUILayout.Button(m_DstDirectory, GUILayout.Width(600)))
             {
                 string dstDirectory = EditorUtility.OpenFolderPanel("选择目标目录", m_DstDirectory, m_DstDirectory);
                 if(!string.IsNullOrEmpty(dstDirectory))
@@ -66,8 +83,15 @@ namespace Application.Editor
             EditorGUILayout.Space();
             if(GUILayout.Button("Extract Anim"))
             {
-                AssetPostprocessorHelper.DoExtractAnimClipBatch(m_SrcDirectory, m_DstDirectory);
+                //AssetPostprocessorHelper.DoExtractAnimClipBatch(m_SrcDirectory, m_DstDirectory);
+                for(int i = 0; i < m_PendingFbxsSelected.Count; ++i)
+                {
+                    if (!m_PendingFbxsSelected[i])
+                        continue;
+                    AssetPostprocessorHelper.ExtractAnimClip(AssetDatabase.GUIDToAssetPath(m_PendingFbxs[i]), m_DstDirectory);
+                }
             }
+            EditorGUILayout.EndScrollView();
         }
     }
 }
