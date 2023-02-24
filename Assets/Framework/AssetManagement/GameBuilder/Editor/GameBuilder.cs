ec3a5a9a0195dd178932c44698b2ea362146380e
xukun <xukunn1226@gmail.com>
2023-02-24 18:19:36 +0800

    #52

--- Assets/Framework/AssetManagement/GameBuilder/Editor/GameBuilder.cs
+++ Assets/Framework/AssetManagement/GameBuilder/Editor/GameBuilder.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.Callbacks;
 using Framework.Core;
 using System.IO;
 #if UNITY_IOS
